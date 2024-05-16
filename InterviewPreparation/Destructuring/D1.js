29f8c27be7e2e11938e7c5527d026e4ca1b0bbac
Abhi <abhikshirsagar1999@gmail.com>
2024-05-16 07:00:38 +0530

    Destructuring Code Added.

---
+++ InterviewPreparation/Destructuring/D1.js
@@ -0,0 +1,27 @@
+const employee = [
+    { nameemp: "A", empid: 1, epmlocation: "Pune" },
+    { nameemp: "B", empid: 2, epmlocation: "Delhi" },
+    { nameemp: "C", empid: 3, epmlocation: "Mubai" },
+    { nameemp: "D", empid: 4, epmlocation: "Noida" }
+]
+function getEmps() {
+    return employee;
+}
+function getEmp(empid) {
+    return employee.find((d) => d.empid == empid)
+}
+const emp = getEmp(1);
+const emp2 = getEmp(2);
+// const empName = emp.nameemp;
+// const location = emp.epmlocation;
+
+const { nameemp, epmlocation } = emp2;
+
+
+
+console.log(nameemp, epmlocation)
+
+
+
+
+// const [em1, em2, em3] = employee;
